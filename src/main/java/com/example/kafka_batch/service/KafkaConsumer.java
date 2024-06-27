88305f1b004a44b45793a144e25d240c9b426439
Bryan Atwood <bryan.atwood@kroger.com>
2024-06-27 12:21:22 -0400

    updates with schedule

--- src/main/java/com/example/kafka_batch/service/KafkaConsumer.java
+++ src/main/java/com/example/kafka_batch/service/KafkaConsumer.java
@@ -1,56 +1,82 @@
 package com.example.kafka_batch.service;
 
+import org.apache.kafka.clients.admin.ListConsumerGroupOffsetsResult;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.common.MetricName;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.metrics.KafkaMetric;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
+import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 
 @Service
 public class KafkaConsumer {
 
-    @KafkaListener(topics = "test-topic", containerFactory = "kafkaListenerContainerFactory")
-    public void receive(Consumer<Long, String> consumer) {
+    private KafkaListenerEndpointRegistry kafkaListenerEndpointRegistry;
+    private LagAnalyzerService lagAnalyzerService;
+    private Consumer consumer;
 
-        final int giveUp = 100;   int noRecordsCount = 0;
-
-        while (true) {
-            final ConsumerRecords<Long, String> consumerRecords = consumer.poll(1000);
-
-            System.out.println(consumerRecords.count());
-
-            if (consumerRecords.count()==0) {
-                noRecordsCount++;
-                if (noRecordsCount > giveUp) break;
-                else continue;
-            }
-
-            consumerRecords.forEach(record -> {
-                System.out.printf("Consumer Record:(%d, %s, %d, %d)\n",
-                        record.key(), record.value(),
-                        record.partition(), record.offset());
-            });
-
-            consumer.commitAsync();
-        }
-        consumer.close();
-
-//        System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
-//        System.out.println("Starting the process to recieve batch messages");
-//
-//        System.out.println("Message Batch Size: " + messages.size());
-//
-//        for (String message: messages) {
-//
-//            System.out.println(message);
-//
-//        }
-//        System.out.println("all the batch messages are consumed");
+    public KafkaConsumer(KafkaListenerEndpointRegistry kafkaListenerEndpointRegistry, LagAnalyzerService lagAnalyzerService) {
+        this.kafkaListenerEndpointRegistry = kafkaListenerEndpointRegistry;
+        this.lagAnalyzerService = lagAnalyzerService;
     }
 
+    @Scheduled(cron = "0 */2 * * * ?")
+    public void scheduledJob(){
+        System.out.println("Job Kicked Off!!!" + LocalDateTime.now());
+
+        var startTime = LocalDateTime.now();
+        var endTime = startTime.plusMinutes(1);
+
+        System.out.println("Job Started!!!" + LocalDateTime.now());
+        kafkaListenerEndpointRegistry.getListenerContainer("test-topic-id").start();
+
+         while(LocalDateTime.now().isBefore(endTime)) {
+
+             try {
+                 Thread.sleep(5000);
+             } catch (InterruptedException e) {
+                 throw new RuntimeException(e);
+             }
+
+         }
+
+        kafkaListenerEndpointRegistry.getListenerContainer("test-topic-id").stop();
+        System.out.println("Job Stopped!!!" + LocalDateTime.now());
+
+    }
+
+    @KafkaListener(id = "test-topic-id", topics = "test-topic", containerFactory = "kafkaListenerContainerFactory", autoStartup = "false")
+    public void receive(@Payload List<String> messages, Consumer consumer) {
+
+        this.consumer = consumer;
+
+        System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+        System.out.println("Starting the process to receive batch messages");
+
+        System.out.println("Message Batch Size: " + messages.size());
+
+        for (String message: messages) {
+
+            System.out.println(message);
+
+        }
+        System.out.println("all the batch messages are consumed");
+    }
 
 }
---
+++ src/main/java/com/example/kafka_batch/service/LagAnalyzerService.java
@@ -0,0 +1,89 @@
+package com.example.kafka_batch.service;
+
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.ListConsumerGroupOffsetsResult;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.concurrent.ExecutionException;
+
+@Service
+public class LagAnalyzerService {
+
+//    @Value("${kafka_broker_url}")
+    private String brokerUrl = "localhost:9092";
+
+    private AdminClient getAdminClient(String bootstrapServerConfig) {
+        Properties config = new Properties();
+        config.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServerConfig);
+        return AdminClient.create(config);
+    }
+
+
+    private Map<TopicPartition, Long> getConsumerGrpOffsets(String groupId)
+            throws ExecutionException, InterruptedException {
+        ListConsumerGroupOffsetsResult info = getAdminClient(brokerUrl).listConsumerGroupOffsets(groupId);
+        Map<TopicPartition, OffsetAndMetadata> topicPartitionOffsetAndMetadataMap = info.partitionsToOffsetAndMetadata().get();
+
+        Map<TopicPartition, Long> groupOffset = new HashMap<>();
+        for (Map.Entry<TopicPartition, OffsetAndMetadata> entry : topicPartitionOffsetAndMetadataMap.entrySet()) {
+            TopicPartition key = entry.getKey();
+            OffsetAndMetadata metadata = entry.getValue();
+            groupOffset.putIfAbsent(new TopicPartition(key.topic(), key.partition()), metadata.offset());
+        }
+        return groupOffset;
+    }
+
+    private KafkaConsumer<String, String> getKafkaConsumer(String bootstrapServerConfig) {
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServerConfig);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        return new KafkaConsumer<>(properties);
+    }
+
+    private Map<TopicPartition, Long> getProducerOffsets(Map<TopicPartition, Long> consumerGrpOffset) {
+        List<TopicPartition> topicPartitions = new LinkedList<>();
+        for (Map.Entry<TopicPartition, Long> entry : consumerGrpOffset.entrySet()) {
+            TopicPartition key = entry.getKey();
+            topicPartitions.add(new TopicPartition(key.topic(), key.partition()));
+        }
+        return getKafkaConsumer(brokerUrl).endOffsets(topicPartitions);
+    }
+
+    private Map<TopicPartition, Long> computeLags(
+            Map<TopicPartition, Long> consumerGrpOffsets,
+            Map<TopicPartition, Long> producerOffsets) {
+        Map<TopicPartition, Long> lags = new HashMap<>();
+        for (Map.Entry<TopicPartition, Long> entry : consumerGrpOffsets.entrySet()) {
+            Long producerOffset = producerOffsets.get(entry.getKey());
+            Long consumerOffset = consumerGrpOffsets.get(entry.getKey());
+            long lag = Math.abs(producerOffset - consumerOffset);
+            lags.putIfAbsent(entry.getKey(), lag);
+        }
+        return lags;
+    }
+
+    public Map<String, Map<Integer, Long>> analyzeLag(String groupId) throws ExecutionException, InterruptedException {
+        Map<String, Map<Integer, Long>> response = new HashMap<>();
+        Map<TopicPartition, Long> consumerGrpOffsets = getConsumerGrpOffsets(groupId);
+        Map<TopicPartition, Long> producerOffsets = getProducerOffsets(consumerGrpOffsets);
+        Map<TopicPartition, Long> lags = computeLags(consumerGrpOffsets, producerOffsets);
+        for (Map.Entry<TopicPartition, Long> lagEntry : lags.entrySet()) {
+            String topic = lagEntry.getKey().topic();
+            int partition = lagEntry.getKey().partition();
+            Long lag = lagEntry.getValue();
+            Map<Integer, Long> topicMap = response.getOrDefault(topic, new HashMap<>());
+            topicMap.put(partition, lag);
+            response.put(topic, topicMap);
+        }
+        return response;
+    }
+}
